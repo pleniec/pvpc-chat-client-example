6bcb85e36a744f1d7f534fb16a25f86bccba3034
Piotr Leniec <leniecpiotr@gmail.com>
2015-07-24 17:15:20 +0200

    printing messages

--- client.js
+++ client.js
@@ -1,4 +1,8 @@
-var accessToken = '55Qgh6AQwP5mK-x7ycUt';
+var accessToken = 'p56ZB21-UeVsGJru4L_N';
 var socket = io.connect('http://localhost:8080?accessToken=' + accessToken);
 
 socket.emit('message', {conversation: 1});
+
+socket.on('message', function(message) {
+  console.log(JSON.stringify(message));
+});
