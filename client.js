b0d9b3d11a67874d1bb4fe1c1a0e6d09f40b9e82
Piotr Leniec <leniecpiotr@gmail.com>
2015-07-27 16:13:17 +0200

    parameterized access token

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-var accessToken = 'cz5WPumy1PkiDg4Xyxzc';
+var accessToken = location.search.match(/\?access_token=(.*)/)[1];
 var socket = io.connect('http://localhost:8080?accessToken=' + accessToken);
 
 socket.on('connect', function() {
